692c0e19bb9af3489b0aa2759bcf15a409af9fc2
Josii13 <caleb2002josias>
2025-11-28 04:33:51 +0100

    updates

--- app/Models/QrCode.php
+++ app/Models/QrCode.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
+
 class QrCode extends Model
 {
     use HasFactory;
